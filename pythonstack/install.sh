1311ba5cc8f2ae6a02e5526388e167dada583001
Pranay Paine <pranaypaine@Pranays-MacBook-Pro.local>
2018-05-08 22:36:42 +0530

    python install script

---
+++ pythonstack/install.sh
@@ -0,0 +1,26 @@
+# Update Packages
+apt-get update
+# Upgrade Packages
+apt-get upgrade
+
+# Basic Linux Stuff
+apt-get install -y git
+
+#install dependencies 
+sudo apt-get install -y python-pip python-dev libmysqlclient-dev
+
+
+# Set MySQL Pass
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password 123456'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 123456'
+
+# Install MySQL
+apt-get install -y mysql-server
+
+# virtual environment
+sudo pip install -y virtualenv
+
+# create virtualenv
+mkdir ~/myproject
+cd ~/myproject
+virtualenv myprojectenv
